bde7d863678b49bb6bf4108a4bc7192bccf98cb0
Drake Redfield <drake.redfield@hotmail.com>
2025-04-22 20:24:39 -0600

    feat: update layout and home components

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,15 +1,13 @@
 import { AiOutlineCalendar, AiOutlineShopping } from "react-icons/ai";
+import Card from "~/components/card";
 
 export default function Home() {
   return <>
     <div className="p-5">
-      <div className="grid md:grid-cols-[1fr_1fr] gap-4">
-        <div className="card">
-          <div className="card-header">
-            <h1>Eventos</h1>
-          </div>
-          <div className="card-body mt-5">
-            {/* <p className="pb-3">No hay eventos registrados este mes</p> */}
+      <div className="grid md:grid-cols-[1fr_1fr] gap-4 mb-5">
+        <Card>
+          <Card.Title>Eventos</Card.Title>
+          <Card.Body>
             {
               [1, 2, 3, 4, 5].map((item) => (
                 <div className="pb-2 mb-2 border-b-1 border-gray-300 gap-2 items-center grid grid-cols-[1fr_auto] md:grid-cols-[auto_1fr_auto] md:gap-4" key={item}>
@@ -22,34 +20,28 @@ export default function Home() {
                 </div>
               ))
             }
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h1>Productos por agotarse</h1>
-          </div>
-          <div className="card-body mt-5">
-            {/* <p className="pb-3">No hay eventos registrados este mes</p> */}
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Title>Productos por agotarse</Card.Title>
+          <Card.Body>
             {
               [1, 2, 3].map((item) => (
                 <div className="pb-2 mb-2 border-b-1 border-gray-300 gap-2 items-center grid grid-cols-[1fr_auto] md:grid-cols-[auto_1fr_auto] md:gap-4" key={item}>
                   <AiOutlineShopping className="hidden md:block text-xl" />
                   <div className="overflow-hidden">
-                    <p className="text-gray-600 text-md mb-1 overflow-hidden whitespace-nowrap text-ellipsis"><strong>Omnilife Magnus</strong></p>
+                    <p className="text-gray-600 text-md overflow-hidden whitespace-nowrap text-ellipsis"><strong>Omnilife Magnus</strong></p>
                   </div>
                   <p className="text-sm text-yellow-500"><strong>Stock: 9</strong></p>
                 </div>
               ))
             }
-          </div>
-        </div>
+          </Card.Body>
+        </Card>
       </div>
-      <div className="card mt-4">
-        <div className="card-header">
-          <h1>Eventos</h1>
-        </div>
-        <div className="card-body mt-5">
-          {/* <p className="pb-3">No hay eventos registrados este mes</p> */}
+      <Card>
+        <Card.Title>Eventos</Card.Title>
+        <Card.Body>
           {
             [1, 2, 3, 4, 5].map((item) => (
               <div className="pb-2 mb-2 border-b-1 border-gray-300 gap-2 items-center grid grid-cols-[1fr_auto] md:grid-cols-[auto_1fr_auto] md:gap-4" key={item}>
@@ -62,8 +54,8 @@ export default function Home() {
               </div>
             ))
           }
-        </div>
-      </div>
+        </Card.Body>
+      </Card>
     </div>
   </>
 }
--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,8 @@
 import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
-  layout('components/layout.tsx',[
-    index("routes/home.tsx")
+  layout('components/layout/index.tsx',[
+    index("routes/home.tsx"),
   ]),
   route('login', 'routes/login.tsx')
 ] satisfies RouteConfig;
